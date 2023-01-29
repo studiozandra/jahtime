82088c28a4fdfd67381d357fa105717431c096bd
studiozandra <zan@studiozandra.com>
2023-01-28 21:50:06 -0500

    fix form action, results to str

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,13 @@
 
 #!python 3.6
 
+from flask import render_template, redirect
 from flask import Flask, redirect, render_template
 from flask import request
 import web_parse
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def jahTime():
@@ -15,36 +17,26 @@ def jahTime():
     if request.method == 'POST' and 'userAge' in request.form:
         userName = request.form.get('userName')
         userAge = request.form.get('userAge')
-        jahAge = str(round(((float(int(userAge) / 0.69444444444444)) / 60) - 0.5)) + str(' hours and ') + str(round(float(int(userAge) / .69444444444444) % 60)) + str(' minutes') # the - 0.5 is to force rounding down
-        #jahAgeDays =
-        #jahAgeHrs =
+        jahAge = str(round(((float(int(userAge) / 0.69444444444444)) / 60) - 0.5)) + str(' hours and ') + str(
+            round(float(int(userAge) / .69444444444444) % 60)) + str(' minutes')  # the - 0.5 is to force rounding down
+        # jahAgeDays =
+        # jahAgeHrs =
     return render_template("index.html",
                            userName=userName,
                            userAge=userAge,
                            jahAge=jahAge)
 
+
 @app.route('/shoppinglist', methods=['GET', 'POST'])
 def shoppingList():
-    url = ''
-    shopping_list = ''
-    
-    if request.method == 'POST' and 'url' in request.form:
-        url = request.form.get('url')
-        shopping_list = web_parse.crawl_GPT(url)
-        print("line 33 reached")
-        return redirect('/shoppinglist')
-        
+    url = 'placeholder'
+    shopping_list = 'placeholder'
 
-    return render_template("shopping-list.html",
-                           url=url,
-                           shopping_list=shopping_list,
-                           )
+    if request.method == 'POST' and ('url' in request.form):
+        url = str(request.form.get('url'))
+        shopping_list = str(web_parse.crawl_GPT(url))
+    return render_template('shoppinglist.html', shopping_list=shopping_list)
 
-
-@app.route('/test', methods=['GET', 'POST'])
-def testing():
-    
-    return "hi mom"
 
 if __name__ == '__main__':
     app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
